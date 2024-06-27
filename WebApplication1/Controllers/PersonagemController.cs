3cb3bafcbec1e96cd4cd6822a3cb5302e10ed08c
BuildTools <unconfigured@null.spigotmc.org>
2024-06-27 17:05:51 -0300

    oi

---
+++ WebApplication1/Controllers/PersonagemController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonagemController : Controller
+    {
+        private BancoTCCEntities db = new BancoTCCEntities();
+
+        // GET: Personagem
+        public ActionResult Index()
+        {
+            return View(db.Personagem.ToList());
+        }
+
+        // GET: Personagem/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personagem personagem = db.Personagem.Find(id);
+            if (personagem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personagem);
+        }
+
+        // GET: Personagem/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personagem/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nome,sexo,classe")] Personagem personagem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Personagem.Add(personagem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(personagem);
+        }
+
+        // GET: Personagem/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personagem personagem = db.Personagem.Find(id);
+            if (personagem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personagem);
+        }
+
+        // POST: Personagem/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nome,sexo,classe")] Personagem personagem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(personagem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(personagem);
+        }
+
+        // GET: Personagem/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personagem personagem = db.Personagem.Find(id);
+            if (personagem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personagem);
+        }
+
+        // POST: Personagem/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Personagem personagem = db.Personagem.Find(id);
+            db.Personagem.Remove(personagem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Models/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Player
+    {
+		private int ID;
+
+		public int id
+		{
+			get { return ID; }
+			set { ID = value; }
+		}
+
+		private string Nome;
+
+		public string nome
+		{
+			get { return Nome; }
+			set { Nome = value; }
+		}
+
+
+		private string Classe;
+
+		public string classe
+		{
+			get { return Classe; }
+			set { Classe = value; }
+		}
+
+		private string Login;
+
+		public string login
+		{
+			get { return Login; }
+			set { Login = value; }
+		}
+
+		private string Senha;
+
+		public string senha
+		{
+			get { return Senha; }
+			set { Senha = value; }
+		}
+
+
+	}
+}
